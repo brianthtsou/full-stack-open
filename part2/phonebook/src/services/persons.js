535ff049a222cef1a0fba2ecf8fab015814ec662
brianthtsou <95939519+brianthtsou@users.noreply.github.com>
2024-04-11 20:05:57 -0700

    completed exercises 3.9-3.11

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 // const baseUrl = "http://localhost:3001/persons";
-// const baseUrl = "/api/persons";
+const baseUrl = "/api/persons";
 
-const baseUrl = "http://localhost:3001/api/persons";
+// const baseUrl = "http://localhost:3001/api/persons";
 
 const getAll = () => {
   return axios.get(baseUrl);
