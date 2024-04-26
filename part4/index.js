f3e2cd82b4da87034e654be18828050c6fc27294
brianthtsou <95939519+brianthtsou@users.noreply.github.com>
2024-04-25 21:03:24 -0700

    exercise 4.2 - created models folder, created and implemented blog.js

--- part4/index.js
+++ part4/index.js
@@ -6,14 +6,7 @@ const mongoose = require("mongoose");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
+const Blog = require("./models/blog");
 
 mongoose.connect(config.MONGODB_URI);
 
