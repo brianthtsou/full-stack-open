16183290ba39561a200f9f266e408ed8c299911c
brianthtsou <95939519+brianthtsou@users.noreply.github.com>
2024-04-25 20:47:25 -0700

    exercise 4.2 - created and implemented config.js

--- part4/index.js
+++ part4/index.js
@@ -3,8 +3,8 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-require("dotenv").config();
 const logger = require("./utils/logger");
+const config = require("./utils/config");
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -15,8 +15,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model("Blog", blogSchema);
 
-const mongoUrl = process.env.MONGODB_URI;
-mongoose.connect(mongoUrl);
+mongoose.connect(config.MONGODB_URI);
 
 app.use(cors());
 app.use(express.json());
@@ -35,7 +34,6 @@ app.post("/api/blogs", (request, response) => {
   });
 });
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
