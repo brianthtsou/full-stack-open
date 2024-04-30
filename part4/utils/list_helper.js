8ddc9c956a72937591617440ebc693a8f7dc5635
brianthtsou <95939519+brianthtsou@users.noreply.github.com>
2024-04-29 17:02:27 -0700

    completed exercise 4.4, added totalLikes test functionality

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const sum = blogs.map((b) => b.likes).reduce((a, b) => a + b);
+  return sum;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
