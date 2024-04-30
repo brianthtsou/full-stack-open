69ffdc647adab12aec7686a72c4e652cd4c26473
brianthtsou <95939519+brianthtsou@users.noreply.github.com>
2024-04-29 17:53:15 -0700

    completed exercise 4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -7,7 +7,22 @@ const totalLikes = (blogs) => {
   return sum;
 };
 
+const mostLikes = (blogs) => {
+  let topBlog = null;
+  let topLikes = 0;
+  const newArray = blogs.map(({ _id, url, __v, ...keepAttrs }) => keepAttrs);
+
+  for (let blog of newArray) {
+    if (blog.likes >= topLikes) {
+      topLikes = blog.likes;
+      topBlog = blog;
+    }
+  }
+  return topBlog;
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  mostLikes,
 };
