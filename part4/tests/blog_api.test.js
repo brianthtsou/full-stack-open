aa30211806be53ce1e1fb08bbb8c894cd7edbaa6
brianthtsou <95939519+brianthtsou@users.noreply.github.com>
2024-05-09 18:13:35 -0700

    completed exercise 4.9 - note the test mongodb uri

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,6 +1,7 @@
 const { test, after } = require("node:test");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
+const assert = require("assert");
 const app = require("../app");
 
 const api = supertest(app);
@@ -10,6 +11,17 @@ test("notes are returned as json", async () => {
     .get("/api/blogs")
     .expect(200)
     .expect("Content-Type", /application\/json/);
+});
+
+test("unique identifier property of blog posts are named id", async () => {
+  const response = await api.get("/api/blogs");
+  const blogs = response.body;
+  console.log(blogs);
+
+  const allHaveId = blogs.every((blog) => blog.hasOwnProperty("id"));
+  assert.strictEqual(allHaveId, true);
+  const allHave_Id = blogs.every((blog) => blog.hasOwnProperty("_id"));
+  assert.strictEqual(allHave_Id, false);
 });
 
 after(async () => {
