ffcbb25df84131011a22fce54efcaf8007901121
brianthtsou <95939519+brianthtsou@users.noreply.github.com>
2024-10-11 20:04:48 -0700

    complete exercise 5.7

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,6 +1,19 @@
+import Togglable from "./Togglable";
+
+const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 2,
+  border: "solid",
+  borderWidth: 1,
+  marginBottom: 5,
+};
+
 const Blog = ({ blog }) => (
-  <div>
+  <div style={blogStyle}>
     {blog.title} {blog.author}
+    <Togglable buttonLabel="view">
+      url: {blog.url} <br></br>likes: {blog.likes} <button>like</button>
+    </Togglable>
   </div>
 );
 
